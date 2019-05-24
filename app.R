143682147c41e96e295fb9317b379f43988ceea0
Alejandro Herrera Patiño <alejandro.herrera.p@multienlace.com.co>
2019-05-24 17:49:17 -0500

    propuesta de las fechas

--- app.R
+++ app.R
@@ -9,45 +9,60 @@ ui <- fluidPage(
   # Titulo App ----
   #titlePanel("Prediccion de accidentes"),
   
-  navbarPage("Prediccion de accidentes",
+  navbarPage("Predicción de accidentes",
              tabPanel("Home", 
                       withTags(
                         div(class="container center",
-                                 h1("Aqui va el video", align="center"),
-                                 div(class="row",
-                                          div(class="col-md-10  col-md-offset-1 videoWrapper",
-                                                   iframe(src = "https://www.youtube.com/embed/G1-HDDDK87M", height="50%", width="100%")
-                                          )
-                                 )
+                            h1("Aquí va el video", align="center"),
+                            div(class="row",
+                                div(class="col-md-10  col-md-offset-1 videoWrapper",
+                                    iframe(src = "https://www.youtube.com/embed/G1-HDDDK87M", height="50%", width="100%")
+                                )
+                            )
                         )
                         
                       )
                       
              ),
-             tabPanel("Historico",
+             tabPanel("Histórico",
                       sidebarLayout(
                         
                         # Sidebar to demonstrate various slider options ----
                         sidebarPanel(
                           
                           # Input: Simple integer interval ----
-                          sliderInput("ano", "Ano:",
+                          sliderInput("ano", "Año:",
                                       min = 2014, max = 2018,
                                       value = 2015),
                           
                           # Input: Specification of range within an interval ----
                           sliderInput("mes", "Mes:",
                                       min = 1, max = 12,
-                                      value = c(2,5))
+                                      value = c(2,5)),
+                          
+                          radioButtons("radio", label = h3("Año"),
+                                       choices = list("2014" = 2014, "2015" = 2015, "2016" = 2016, "2017" = 2017), 
+                                       selected = 2015),
+                          
+                          dateRangeInput("dates", label = h3("Rango de fechas"))
                         ),
                         
                         # Main panel for displaying outputs ----
                         mainPanel(
                           
                           # Output: Table summarizing the values entered ----
-                          tableOutput("values")
+                          tableOutput("values"),
                           
-                        )
+                          
+                          fluidRow(verbatimTextOutput("fecha")),
+                          
+                          
+                          
+                          fluidRow(verbatimTextOutput("radio"))
+                          
+                        ),
+                        
+                        
                       )
              ),
              navbarMenu('Menu1',
@@ -61,7 +76,11 @@ ui <- fluidPage(
 
 #logica de la app
 server <- function(input, output) {
+  # You can access the values of the widget (as a vector of Dates)
+  # with input$dates, e.g.
+  output$fecha <- renderPrint({ input$dates })
   
+  output$radio <- renderPrint({ input$radio })
   
 }
 
